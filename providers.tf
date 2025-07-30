80c42a0d52dee81da40f8e950a782191468f4bfa
Guilherme Garcia <garciaguig@gmail.com>
2025-07-30 19:35:31 -0300

    chore: implement aws 'iam' module initial structure

--- providers.tf
+++ providers.tf
@@ -10,4 +10,10 @@ terraform {
 provider "aws" {
   # Profile será lido automaticamente da variável de ambiente AWS_PROFILE
   # Caso não tenha configurado, execute em seu terminal: export AWS_PROFILE=seu_profile
+
+  default_tags {
+    tags = {
+      IAC = "True"
+    }
+  }
 }
