ddcdea5e3000fdac56bc6795cfe2f3727ea91738
Guilherme Garcia <garciaguig@gmail.com>
2025-08-02 17:56:51 -0300

    chore: configure aws iam tf role

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -5,3 +5,61 @@ resource "aws_iam_openid_connect_provider" "oidc-git" {
     "sts.amazonaws.com"
   ]
 }
+
+resource "aws_iam_role" "tf-role" {
+  name = "tf-role"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Condition" : {
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:aud" : [
+              "sts.amazonaws.com"
+            ]
+          },
+          "StringLike" : {
+            "token.actions.githubusercontent.com:sub" : [
+              "repo:garciaagui/rocketseat-devops:ref:refs/heads/main"
+            ]
+          }
+        }
+        "Effect" : "Allow",
+        "Principal" : {
+          "Federated" : "arn:aws:iam::350124735268:oidc-provider/token.actions.githubusercontent.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "tf-role-policy" {
+  name = "tf-role-policy"
+  role = aws_iam_role.tf-role.id
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "Statement1",
+        "Action" : "ecr:*",
+        "Effect" : "Allow",
+        "Resource" : "*"
+      },
+      {
+        "Sid" : "Statement2",
+        "Action" : "iam:*",
+        "Effect" : "Allow",
+        "Resource" : "*"
+      },
+      {
+        "Sid" : "Statement3",
+        "Action" : "s3:*",
+        "Effect" : "Allow",
+        "Resource" : "*"
+      }
+    ]
+  })
+}
