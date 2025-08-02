0e5112d50c03a8f574c742599ced8a5aec591c26
Guilherme Garcia <garciaguig@gmail.com>
2025-08-02 17:06:35 -0300

    chore: implement aws state and versioning buckets

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
 module "iam" {
   source = "./modules/iam"
 }
+
+module "s3" {
+  source = "./modules/s3"
+}
---
+++ modules/s3/main.tf
@@ -0,0 +1,17 @@
+resource "aws_s3_bucket" "state_bucket" {
+  bucket = "solucione-state-bucket"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning_bucket" {
+  bucket = aws_s3_bucket.state_bucket.bucket
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+
+  depends_on = [aws_s3_bucket.state_bucket]
+}
