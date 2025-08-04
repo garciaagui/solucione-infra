ff565a19674ab669e55c24d3bb68b0799d6a21d6
Guilherme Garcia <garciaguig@gmail.com>
2025-08-04 20:42:03 -0300

    chore: create ecr role on iam module

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -40,6 +40,35 @@ resource "aws_iam_role" "tf-role" {
   })
 }
 
+resource "aws_iam_role" "ecr-role" {
+  name = "ecr-role"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Principal" : {
+          "Federated" : "arn:aws:iam::350124735268:oidc-provider/token.actions.githubusercontent.com"
+        },
+        "Condition" : {
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:aud" : [
+              "sts.amazonaws.com"
+            ]
+          },
+          "StringLike" : {
+            "token.actions.githubusercontent.com:sub" : [
+              "repo:garciaagui/solucione-infra:ref:refs/heads/main"
+            ]
+          }
+        }
+      }
+    ]
+  })
+}
+
 resource "aws_iam_role_policy" "tf-role-policy" {
   name = "tf-role-policy"
   role = aws_iam_role.tf-role.id
@@ -62,6 +91,47 @@ resource "aws_iam_role_policy" "tf-role-policy" {
       {
         "Sid" : "Statement3",
         "Action" : "s3:*",
+        "Effect" : "Allow",
+        "Resource" : "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "ecr-role-policy" {
+  name = "ecr-role-policy"
+  role = aws_iam_role.ecr-role.id
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "Statement1",
+        "Action" : "apprunner:*",
+        "Effect" : "Allow",
+        "Resource" : "*"
+      },
+      {
+        "Sid" : "Statement2",
+        "Action" : [
+          "iam:PassRole",
+          "iam:CreateServiceLinkedRole",
+        ],
+        "Effect" : "Allow",
+        "Resource" : "*"
+      },
+      {
+        "Sid" : "Statement3",
+        "Action" : [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload",
+          "ecr:GetAuthorizationToken",
+        ]
         "Effect" : "Allow",
         "Resource" : "*"
       }
