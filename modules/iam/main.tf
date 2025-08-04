bfe007c60a5e708a20b9cb49a6a08cc6097a4bc4
Guilherme Garcia <garciaguig@gmail.com>
2025-08-04 18:45:48 -0300

    fix: fix thumbprint list values on iam aws oidc resource

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -6,7 +6,8 @@ resource "aws_iam_openid_connect_provider" "oidc-git" {
   ]
 
   thumbprint_list = [
-    "1b511abead59c6ce207077c0bf0e0043b1382612"
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd"
   ]
 }
 
