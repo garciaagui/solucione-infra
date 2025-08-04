285d9ef74f79ae9a842a7bb4249569311ff20917
Guilherme Garcia <garciaguig@gmail.com>
2025-08-04 18:39:22 -0300

    fix: add thumbprint list to aws oidc resource

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -4,6 +4,10 @@ resource "aws_iam_openid_connect_provider" "oidc-git" {
   client_id_list = [
     "sts.amazonaws.com"
   ]
+
+  thumbprint_list = [
+    "1b511abead59c6ce207077c0bf0e0043b1382612"
+  ]
 }
 
 resource "aws_iam_role" "tf-role" {
